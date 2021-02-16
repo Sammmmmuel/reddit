367474a4f688d76bed968afaead92ae560a0f4d0
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-02-15 23:53:34 -0800

    User can submit and save posts

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
-    //Loads the handlebars module
-const handlebars = require('express-handlebars');
-//Sets our app to use the handlebars engine
-app.set('view engine', 'handlebars');
+const hbs = require('express-handlebars');
+const path = require('path')
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
-//Sets handlebars configurations (we will go through them later on)
-app.engine('handlebars', handlebars({
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+app.engine('hbs', hbs({
     layoutsDir: __dirname + '/views/layouts',
+    defaultLayout: 'index',
+    extname: 'hbs'
 }));
-
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
     res.render('main', { layout: 'index' });
 });
 
+app.get("/posts/new", (req, res) => {
+    res.render("posts-new.hbs")
+})
+
+
+
+require('./controllers/posts.js')(app);
+// Set db
+require('./data/reddit-db');
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
