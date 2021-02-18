ae4fe37b5f42c00efb9602b3fdf7eb243b3f033f
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-02-17 18:47:22 -0800

    implemented remaining authentication tests

--- test/posts.js
+++ test/posts.js
@@ -90,3 +90,22 @@ describe('Posts', function() {
             });
     });
 });
+// login
+it("should be able to login", function(done) {
+    agent
+        .post("/login")
+        .send({ username: "testone", password: "password" })
+        .end(function(err, res) {
+            res.should.have.status(200);
+            agent.should.have.cookie("nToken");
+            done();
+        });
+});
+// logout
+it("should be able to logout", function(done) {
+    agent.get("/logout").end(function(err, res) {
+        res.should.have.status(200);
+        agent.should.not.have.cookie("nToken");
+        done();
+    });
+});
