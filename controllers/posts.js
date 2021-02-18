535800ff44919118a2478f78fbb4e69fc02ae3a0
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-02-17 22:49:27 -0800

    users can now vote on posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,8 +22,10 @@ module.exports = (app) => {
         if (req.user) {
             const user = req.user
             const post = new Post(req.body);
-            // post.author = req.user._id;
-            post.author = user._id
+            post.author = req.user._id;
+            post.upVotes = [];
+            post.downVotes = [];
+            post.voteScore = 0;
             post
                 .save()
                 .then(post => {
@@ -79,4 +81,23 @@ module.exports = (app) => {
                 console.log(err.message);
             })
     })
+    app.put("/posts/:id/vote-up", function(req, res) {
+        Post.findById(req.params.id).exec(function(err, post) {
+            post.upVotes.push(req.user._id);
+            post.voteScore = post.voteScore + 1;
+            post.save();
+
+            res.status(200);
+        });
+    });
+
+    app.put("/posts/:id/vote-down", function(req, res) {
+        Post.findById(req.params.id).exec(function(err, post) {
+            post.downVotes.push(req.user._id);
+            post.voteScore = post.voteScore - 1;
+            post.save();
+
+            res.status(200);
+        });
+    });
 };
